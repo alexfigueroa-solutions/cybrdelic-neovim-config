8f20d2048eb5635a03942aea69979b7b89ee9989
cybrvybe <alexfigueroa.cybr@gmail.com>
2024-10-04 19:59:46 -0700

    Expanded NeoTree configuration in init.lua with additional settings, components, and key mappings

--- init.lua
+++ init.lua
@@ -130,12 +130,98 @@ require('lazy').setup {
     },
     config = function()
       require('neo-tree').setup {
+        close_if_last_window = true,
+        popup_border_style = 'rounded',
+        enable_git_status = true,
+        enable_diagnostics = true,
+        sort_case_insensitive = true,
+        default_component_configs = {
+          indent = {
+            indent_size = 2,
+            padding = 1,
+            with_markers = true,
+            indent_marker = '│',
+            last_indent_marker = '└',
+            highlight = 'NeoTreeIndentMarker',
+          },
+          icon = {
+            folder_closed = '',
+            folder_open = '',
+            folder_empty = '',
+            default = '*',
+            highlight = 'NeoTreeFileIcon',
+          },
+          modified = {
+            symbol = '[+]',
+            highlight = 'NeoTreeModified',
+          },
+          name = {
+            trailing_slash = false,
+            use_git_status_colors = true,
+            highlight = 'NeoTreeFileName',
+          },
+          git_status = {
+            symbols = {
+              added = '',
+              modified = '',
+              deleted = '✖',
+              renamed = '',
+              untracked = '',
+              ignored = '',
+              unstaged = '',
+              staged = '',
+              conflict = '',
+            },
+          },
+        },
         window = {
           position = 'left',
-          width = 30,
+          width = 40,
           mapping_options = {
             noremap = true,
             nowait = true,
+          },
+          mappings = {
+            ['<space>'] = {
+              'toggle_node',
+              nowait = false, -- disable 'nowait' if you have existing combos starting with this char that you want to use
+            },
+            ['<2-LeftMouse>'] = 'open',
+            ['<cr>'] = 'open',
+            ['S'] = 'open_split',
+            ['s'] = 'open_vsplit',
+            ['t'] = 'open_tabnew',
+            ['w'] = 'open_with_window_picker',
+            ['C'] = 'close_node',
+            ['a'] = 'add',
+            ['A'] = 'add_directory',
+            ['d'] = 'delete',
+            ['r'] = 'rename',
+            ['y'] = 'copy_to_clipboard',
+            ['x'] = 'cut_to_clipboard',
+            ['p'] = 'paste_from_clipboard',
+            ['c'] = 'copy',
+            ['m'] = 'move',
+            ['q'] = 'close_window',
+          },
+        },
+        filesystem = {
+          filtered_items = {
+            visible = true, -- This will show hidden files
+            hide_dotfiles = false,
+            hide_gitignored = false,
+          },
+          follow_current_file = true,
+          use_libuv_file_watcher = true,
+        },
+        buffers = {
+          follow_current_file = true,
+          group_empty_dirs = true,
+          show_unloaded = true,
+        },
+        git_status = {
+          window = {
+            position = 'float',
           },
         },
       }
@@ -527,6 +613,7 @@ function _G.change_focus()
     vim.cmd 'bnext'
   end
 end
+
 map('n', '<leader><Tab>', '<cmd>lua change_focus()<CR>', { desc = 'Change focus globally' })
 
 -- Formatting keybinding
