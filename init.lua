2707b2533542c26bc50b21bf970c2007278da771
cybrvybe <alexfigueroa.cybr@gmail.com>
2024-10-10 07:38:13 -0700

    Add avante_lib and CopyErrors command; bind <leader>ce for error clipboard copying in init.lua

--- init.lua
+++ init.lua
@@ -8,8 +8,10 @@ vim.g.have_nerd_font = true
 -- Load settings
 require 'settings'
 
+
 -- Load plugins
 require 'plugins'
+
 
 -- Load keybindings
 require 'keybindings'
@@ -28,6 +30,11 @@ require 'analysis'
 
 -- Load autocmds
 require 'autocmds'
+
+-- Load avante_lib (if applicable)
+require('avante_lib').load()
+
+
 
 -- Set initial colorscheme
 vim.cmd.colorscheme 'tokyonight-night'
@@ -48,6 +55,13 @@ require('tokyonight').setup {
     colors.bg_statusline = 'NONE'
   end,
 }
+-- Define a user command to copy errors to the clipboard
+vim.api.nvim_create_user_command('CopyErrors', function()
+  require('utils').copy_errors_to_clipboard()
+end, {})
+
+-- Optionally bind a key to run the command
+vim.keymap.set('n', '<leader>ce', ':CopyErrors<CR>', { desc = 'Copy all error logs to clipboard' })
 
 -- Ensure transparency is set after colorscheme
 vim.schedule(function()
@@ -56,10 +70,6 @@ end)
 
 -- Load ANTHROPIC_API_KEY from ~/.zshrc
 require('utils').load_anthropic_api_key()
-
--- Load avante_lib (if applicable)
--- require('avante_lib').load()
-
 -- Command to open notes
 vim.api.nvim_create_user_command('Notes', require('notes').open_notes, {})
 
