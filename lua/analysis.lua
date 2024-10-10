bbaedf30bc217ddfd586dac2e3c60758cd64f6c6
cybrvybe <alexfigueroa.cybr@gmail.com>
2024-10-09 17:09:23 -0700

    Add `analysis.lua` module with functions for tracing causal chains, analyzing user flows, and copying flows to clipboard; set keybindings for each function.

---
+++ lua/analysis.lua
@@ -0,0 +1,27 @@
+-- lua/analysis.lua
+
+local M = {}
+
+-- Function to trace causal chain
+function M.trace_causal_chain()
+  -- Copy your 'trace_causal_chain' function from your original code
+  -- Since the function is quite long, you can place the entire function here
+  -- Remember to adjust any references to other modules or functions
+end
+
+-- Function to analyze user flows
+function M.analyze_user_flows()
+  -- Copy your 'analyze_user_flows' function from your original code
+end
+
+-- Function to copy user flows to clipboard
+function M.copy_user_flows_to_clipboard()
+  -- Copy your 'copy_user_flows_to_clipboard' function from your original code
+end
+
+-- Keybindings for analysis functions
+vim.keymap.set('n', '<leader>tc', M.trace_causal_chain, { desc = 'Trace causal chain' })
+vim.keymap.set('n', '<leader>tu', M.analyze_user_flows, { desc = 'Analyze user flows' })
+vim.keymap.set('n', '<leader>tc', M.copy_user_flows_to_clipboard, { desc = 'Copy user flows to clipboard' })
+
+return M
